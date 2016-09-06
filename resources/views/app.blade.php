077f0e88c1f5cc9cc0bbdf04ce2d104a0c95ea7f
Arief Setya <ariefsetya@live.com>
2016-09-06 17:00:27 +0700

    moving js to bottom

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,56 +20,12 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="{{url('assets/css/additional.css')}}">
 <!--//fonts-->  
-<!-- js -->
-<script type="text/javascript" src="{{url('assets/js/jquery.min.js')}}"></script>
-<!-- js -->
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="{{url('assets/js/bootstrap.min.js')}}"></script>
-<script src="{{url('assets/js/bootstrap-select.js')}}"></script>
-<script>
-  $(document).ready(function () {
-    var mySelect = $('#first-disabled2');
-
-    $('#special').on('click', function () {
-      mySelect.find('option:selected').prop('disabled', true);
-      mySelect.selectpicker('refresh');
-    });
-
-    $('#special2').on('click', function () {
-      mySelect.find('option:disabled').prop('disabled', false);
-      mySelect.selectpicker('refresh');
-    });
-
-    $('#basic2').selectpicker({
-      liveSearch: true,
-      maxOptions: 1
-    });
-  });
-</script>             
-<script type="text/javascript" src="{{url('assets/js/jquery-ui.js')}}"></script>              
-<script type="text/javascript" src="{{url('assets/js/jquery.price.format.js')}}"></script>              
-<script type="text/javascript" src="{{url('assets/js/jquery.leanModal.min.js')}}"></script>
+        
 <link href="{{url('assets/css/jquery.uls.css')}}" rel="stylesheet"/>
 <link href="{{url('assets/css/jquery.uls.grid.css')}}" rel="stylesheet"/>
 <link href="{{url('assets/css/jquery.uls.lcd.css')}}" rel="stylesheet"/>
 <!-- Source -->
-<script src="{{url('assets/js/jquery.uls.data.js')}}"></script>
-<script src="{{url('assets/js/jquery.uls.data.utils.js')}}"></script>
-<script src="{{url('assets/js/jquery.uls.lcd.js')}}"></script>
-<script src="{{url('assets/js/jquery.uls.languagefilter.js')}}"></script>
-<script src="{{url('assets/js/jquery.uls.regionfilter.js')}}"></script>
-<script src="{{url('assets/js/jquery.uls.core.js')}}"></script>
-<script>
-  $( document ).ready( function() {
-    $( '.uls-trigger' ).uls( {
-      onSelect : function( language ) {
-        var languageName = $.uls.data.getAutonym( language );
-        $( '.uls-trigger' ).text( languageName );
-      },
-      quickList: ['en', 'hi', 'he', 'ml', 'ta', 'fr'] //FIXME
-    } );
-  } );
-</script>
+
 @yield('header')
 </head>
 <body>
@@ -92,6 +48,52 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     @yield('body')
     <footer>
       @yield('footer')
+      <!-- js -->
+      <script async type="text/javascript" src="{{url('assets/js/jquery.min.js')}}"></script>
+      <!-- js -->
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script async src="{{url('assets/js/bootstrap.min.js')}}"></script>
+      <script async src="{{url('assets/js/bootstrap-select.js')}}"></script>
+      <script>
+        $(document).ready(function () {
+          var mySelect = $('#first-disabled2');
+
+          $('#special').on('click', function () {
+            mySelect.find('option:selected').prop('disabled', true);
+            mySelect.selectpicker('refresh');
+          });
+
+          $('#special2').on('click', function () {
+            mySelect.find('option:disabled').prop('disabled', false);
+            mySelect.selectpicker('refresh');
+          });
+
+          $('#basic2').selectpicker({
+            liveSearch: true,
+            maxOptions: 1
+          });
+        });
+      </script>     
+      <script async type="text/javascript" src="{{url('assets/js/jquery-ui.js')}}"></script>              
+      <script async type="text/javascript" src="{{url('assets/js/jquery.price.format.js')}}"></script>              
+      <script async type="text/javascript" src="{{url('assets/js/jquery.leanModal.min.js')}}"></script>
+      <script async src="{{url('assets/js/jquery.uls.data.js')}}"></script>
+      <script async src="{{url('assets/js/jquery.uls.data.utils.js')}}"></script>
+      <script async src="{{url('assets/js/jquery.uls.lcd.js')}}"></script>
+      <script async src="{{url('assets/js/jquery.uls.languagefilter.js')}}"></script>
+      <script async src="{{url('assets/js/jquery.uls.regionfilter.js')}}"></script>
+      <script async src="{{url('assets/js/jquery.uls.core.js')}}"></script>
+      <script>
+        $( document ).ready( function() {
+          $( '.uls-trigger' ).uls( {
+            onSelect : function( language ) {
+              var languageName = $.uls.data.getAutonym( language );
+              $( '.uls-trigger' ).text( languageName );
+            },
+            quickList: ['en', 'hi', 'he', 'ml', 'ta', 'fr'] //FIXME
+          } );
+        } );
+      </script>
       <script type="text/javascript">
         
         jQuery.each(jQuery('textarea[data-autoresize]'), function() {
